5e33e01856053273a47be2afa828c99bf54b029e
siddhant94kaushik <skaushik@calance.com>
2023-06-13 12:14:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,39 +5,12 @@ import React, { useState } from 'react';
 function App() {
 
   const [temperatureValue, setTemperatureValue] = useState(0);
-  // const [temperatureColor, setTemperatureColor] = useState('blue');
-
   
-
-  // const handleIncrementTemp = () => {
-    
-  //   let limitTemperatureValue = temperatureValue + 1;
-  //   setTemperatureValue(limitTemperatureValue);
-
-  //   // if(limitTemperatureValue >= 18){
-  //   //   setTemperatureColor("red");
-  //   // }
-    
-  // }
-
-  // const handleDecrementTemp = () => {
-    
-  //   let limitTemperatureValue = temperatureValue - 1;
-  //   setTemperatureValue(limitTemperatureValue);
-  // }
 
   const handleTemperatureChange = (event) => {
     const value = event.target.value;
     setTemperatureValue(value);
   };
-
-  // const temperatureColor = temperatureValue >= 18 ? 'red' : 'blue';
-
- // code optimisation point below :--
- //  single place for temperature call.(use it in template).
- //  slider use for it.
- //  function ek hi call on both the button call, identify which function to call conditionlly.
-//  Above all are optimized........
 
 
   return (
@@ -49,11 +22,6 @@ function App() {
           {temperatureValue}C
 
         </div>
-        {/* <div className="button-container">
-          <button type="button" className="btn btn-outline-info" onClick={handleIncrementTemp}>+</button>
-          <button type="button" className="btn btn-outline-info" onClick={handleDecrementTemp}>-</button>
-        </div> */}
-
         <div className="slider-container">
           <input
             type="range"
